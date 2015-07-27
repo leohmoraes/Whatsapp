e5fb74310f3caad90aeb2554cccfc6acf9771798
Léo Moraes <leohmoraes@users.noreply.github.com>
2015-07-27 12:47:30 -0300

    Exemplo para envio uma lista de mensagens
    TODO: .getJSON (url_site) to send message

--- Basic Spam.js
+++ Basic Spam.js
@@ -7,12 +7,20 @@ function dispatch(target, eventType, char) {
    target.dispatchEvent(evt);
 }
 function spam(){
-  if (contador1<100){   // How many times spam will exexcute
-    dispatch(campo, "textInput", "You are being spammed by JS"); // Msg to be texted 
-    var input = document.getElementsByClassName("icon btn-icon icon-send"); //Grabs the send button
-    input[0].click(); // Clicks the send button
-    contador1++;
-    setTimeout(spam,1); // Add delay to the recursive function, otherwise it won't work
-  }
+    var mensagens="Embora ninguém possa voltar atrás e fazer um novo começo, qualquer um pode começar agora e fazer um novo fim...|Quando somos bons para os outros, somos melhores para nós mesmos.|Algumas coisas não precisam fazer sentido, basta valer a pena.|O primeiro passo para se chegar a algum lugar é decidir que você não quer mais ficar onde está.";  
+
+    var array_msg = mensagens.split('|');
+    var total_mensagens = array_msg.length;
+    console.log("total: "+total_mensagens);
+
+    if (contador1< total_mensagens){   // How many times spam will exexcute
+      var msg = array_msg[contador1];
+      console.log(contador1+ ") " +msg);
+      dispatch(campo, "textInput", msg); // Msg to be texted
+      var input = document.getElementsByClassName("icon btn-icon icon-send"); //Grabs the send button
+      input[0].click(); // Clicks the send button
+      contador1++;
+      setTimeout(spam,1); // Add delay to the recursive function, otherwise it won't work
+    }
 }
 spam();
